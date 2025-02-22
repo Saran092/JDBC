6ba5c64aead0383d6bc882716443059a7f6e8a33
Saran K <146074509+Saran092@users.noreply.github.com>
2025-02-22 16:21:09 +0530

    #Connection Class to DB

---
+++ CreateCon.java
@@ -0,0 +1,15 @@
+public class CreateCon {
+    private String url="jdbc:mysql://localhost:3306/jdbcdemo",username="root",password ="Saran6112@";
+
+    public String getUrl() {
+        return url;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public CreateCon() {
+    }
+}
